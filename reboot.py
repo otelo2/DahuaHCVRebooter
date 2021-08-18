3c3aca2d3cc9f90fb6f14f1c99b07c22062f3c25
Otelo2 <josean2009@live.com.mx>
2021-08-18 00:58:15 -0500

    Trying to figure out how to keep the host offline

--- reboot.py
+++ reboot.py
@@ -1,20 +1,63 @@
 #!/usr/bin/env python
 #https://gist.github.com/gxfxyz/48072a72be3a169bc43549e676713201#file-dahua_rpc-py
-from dahua_rpc import DahuaRpc
+import time
+from dahua_rpc import DahuaRpc, RebootDone
+from Ping import ping
 
 host = "192.168.100.208"
 username = "admin"
 password = "admin"
 
-dahua = DahuaRpc(host, username, password)
-print(f"Attempting to log into {host} with {username}:{password}")
-dahua.login()
-print(f"Correctly logged in at {dahua.current_time()}")
-selection = input("Do you really want to reboot the device? (y/n) \n").lower()
-if selection == 'y':
-    print("Rebooting...")
-    dahua.reboot()
-    exit()
+def firstReboot():
+    dahua = DahuaRpc(host, username, password)
+    print(f"Attempting to log into {host} with {username}:{password}")
+    dahua.login()
+    print(f"Correctly logged in at {dahua.current_time()}")
+    selection = input("Do you really want to reboot the device? (y/n) \n").lower()
+    if selection == 'y':
+        print("Rebooting...")
+        timeStart = time.time()
+        try:
+            dahua.reboot()
+        except RebootDone:
+            print("DVR likely reboted!")
+        
+        #Check if the DVR has come online
+        DOS = input("Do you want to keep rebooting the device? (y/n) ").lower()
+        if DOS == "y":
+            while(ping(host)==False):
+                pass
+            timeEnd = time.time()
+            print(f"DVR is back online after {timeEnd-timeStart} seconds")
+            DOS_reboot()
+        else:
+            print("Exiting reboot sequence")
 
-print("Goodbye")
-exit()
+def DOS_reboot():
+    dahua = DahuaRpc(host, username, password)
+    print(f"Attempting to log into {host} with {username}:{password}")
+    dahua.login()
+    print(f"Correctly logged in at {dahua.current_time()}")
+
+    print("Rebooting...")
+    timeStart = time.time()
+    try:
+        dahua.reboot()
+    except RebootDone:
+        print("DVR likely reboted!")
+        
+        #Check if the DVR has come online
+        while(ping(host)==False):
+            pass
+        timeEnd = time.time()
+        print(f"DVR is back online after {timeEnd-timeStart} seconds")
+        print("Remember to ctrl + C to stop rebooting")
+        DOS_reboot()
+        
+
+def main():
+    firstReboot()
+    print("Goodbye")
+
+if __name__ == "__main__":
+    main()
